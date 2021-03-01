0fde7420f12f06c97b03bf2f75e8c259a4b4559b
diliprathod32 <diliprathod32@gmail.com>
2021-03-01 22:31:09 +0530

    [Dilip] WelcomeToStackAndQueueOperations

---
+++ StackAndQueue/LinkedListStack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    class LinkedListStack
+    {
+        Node top;
+        /// <summary>
+        /// push operation in stack
+        /// </summary>
+        /// <param name="newData"></param>
+        public void Push(int newData)
+        {
+            Node newNode = new Node(newData);
+            if (this.top != null)
+            {
+                newNode.next = this.top;
+            }
+            this.top = newNode;
+            Console.WriteLine("New Node added: "+newData);
+        }
+       
+    }
+}
---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Operations");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Display();
+            Console.WriteLine("Pushing nodes in stack");
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            Console.ReadLine();
+        }
+    }
+}
