15eb8ae45ee120a0a749e0e0034ef4dc2c4eadb2
diliprathod32 <diliprathod32@gmail.com>
2021-03-01 22:33:27 +0530

    [Dilip] PushNodesInStack

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -8,12 +8,12 @@ namespace StackAndQueue
         {
             Console.WriteLine("Welcome to Stack and Queue Operations");
             LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Display();
+            //UC-1
             Console.WriteLine("Pushing nodes in stack");
             linkedListStack.Push(70);
             linkedListStack.Push(30);
             linkedListStack.Push(56);
-            linkedListStack.Display();
+           
             Console.ReadLine();
         }
     }
